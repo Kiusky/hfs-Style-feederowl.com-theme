2b4dfe196dc553dc9523dcecb3371212cffdf95f
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-09-23 11:20:34 -0500

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,6 +1,6 @@
 exports.version = 0.5
 exports.description = "Style Dark Mode Theme [Feeder Owl]"
 exports.apiRequired = 1
-exports.repo = "Kiusky/hfs-Style-feederowl.com"
+exports.repo = "Kiusky/hfs-Style-feederowl.com-theme"
 exports.frontend_css = 'feederowl.css'
 exports.preview = ["https://github.com/user-attachments/assets/5e6ea1e8-8dc5-4040-9fbb-38fbffbe7ae5", "https://github.com/user-attachments/assets/50e1358e-176e-447d-9b37-8c32a25ecd63"]
