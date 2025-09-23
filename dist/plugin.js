df984aa5ddc825f73c79323637004d88ac1c420b
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-09-22 23:51:55 -0500

    Add files via upload

---
+++ dist/plugin.js
@@ -0,0 +1,6 @@
+exports.version = 0.5
+exports.description = "Style Dark Mode Theme [Feeder Owl]"
+exports.apiRequired = 1
+exports.repo = "Kiusky/hfs-Style-feederowl.com"
+exports.frontend_css = 'feederowl.css'
+exports.preview = ["https://github.com/user-attachments/assets/da76e5f9-2e4d-4ce7-a5b3-a06c74c7ca9f", "https://github.com/user-attachments/assets/be57e3d5-9830-451a-acd6-a94f18800c9a"]
