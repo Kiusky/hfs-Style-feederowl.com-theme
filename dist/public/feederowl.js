241041621cbc7c1df6e940d4a33c1884c61e2a11
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-09-26 14:10:03 -0500

    Add files via upload

---
+++ dist/public/feederowl.js
@@ -0,0 +1,17 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Backspace' && !['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) {
+        event.preventDefault();
+        history.back();
+    }
+});
+
+window.addEventListener("orientationchange", () => {
+  const isLandscape = screen.orientation?.angle === 90 || screen.orientation?.angle === -90;
+
+  if (isLandscape) {
+    const video = document.querySelector("video");
+    if (video && video.requestFullscreen) {
+      video.requestFullscreen().catch((e) => console.warn("Erro ao tentar tela cheia:", e));
+    }
+  }
+});
